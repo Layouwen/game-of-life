0dfcefb5ab096234d3bd868cb30ecc85a630eba4
layouwen <layouwen@gmail.com>
2021-02-14 20:53:14 +0800

    complete course and community static page

---
+++ src/pages/community/community.vue
@@ -0,0 +1,67 @@
+<template>
+  <view class="community_main_wrapper">
+    <view class="main">
+      <view
+        class="card"
+        v-for="(item, index) in cardList"
+        :key="index"
+      >
+        <image class="avatar" src="" />
+        <text class="name">努力了吗梁同学</text>
+        <view class="content"></view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class community extends Vue {
+  cardList = [1, 2, 3, 4];
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/fn.scss";
+
+.community_main_wrapper {
+  overflow: auto;
+  height: 100vh;
+  padding: rpx(12);
+  > .main {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    > .card {
+      $card: rpx(170);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      overflow: hidden;
+      width: $card;
+      min-height: $card;
+      margin-bottom: rpx(10);
+      border-radius: rpx(10);
+      border: rpx(1) solid #000;
+      > .avatar {
+        $avatar: rpx(50);
+        width: $avatar;
+        height: $avatar;
+        margin-top: rpx(10);
+        border-radius: 50%;
+        background: skyblue;
+      }
+      > .name {
+        margin: rpx(10) 0;
+      }
+      > .content {
+        flex-grow: 1;
+        width: 100%;
+        border-top: rpx(1) solid #000;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/course/course.vue
@@ -0,0 +1,73 @@
+<template>
+  <Layout class="course_main_wrapper" class-style="layout">
+    <view
+      class="card"
+      v-for="(item, index) in cardList"
+      :key="index"
+      @click="onLinkPage"
+    >
+      <image class="logo" src="" />
+      <text class="title">成长</text>
+      <text class="count">10/100</text>
+      <text class="percent">10%</text>
+    </view>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Layout from '@/components/Layout.vue';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {Layout},
+})
+export default class course extends Vue {
+  cardList = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  onLinkPage () {
+    uni.navigateTo({url: '/pages/course/detail'});
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/fn.scss";
+
+::v-deep .layout {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: rpx(12);
+}
+
+.course_main_wrapper {
+  .card {
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    width: rpx(349);
+    height: rpx(94);
+    padding: rpx(20);
+    margin-bottom: rpx(10);
+    font-size: rpx(20);
+    border: rpx(1) solid #707070;
+    border-radius: rpx(10);
+    background: #fff;
+    > .logo {
+      $logo: rpx(54);
+      width: $logo;
+      height: $logo;
+      margin-right: rpx(20);
+      border: rpx(1) solid #707070;
+      border-radius: rpx(10);
+      background: #FFFFFF;
+    }
+    > .title {
+      flex-grow: 1;
+    }
+    > .count {
+      margin: 0 rpx(20);
+    }
+  }
+}
+</style>
