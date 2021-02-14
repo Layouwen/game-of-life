3412d295d0bc4a8784d647a5eedd2b5f29b61660
layouwen <layouwen@gmail.com>
2021-02-14 20:50:42 +0800

    add component the Layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="layout_wrapper" :class="classStyle">
+    <slot />
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Layout extends Vue {
+  @Prop(String) readonly classStyle !: String;
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/fn.scss";
+
+.layout_wrapper {
+  height: 100vh;
+  overflow: auto;
+}
+</style>
